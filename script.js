464a27e8e3831ed4dd7d58fc7908dc88ac0dacc1
muhammedNecatiPolat <muhammed.polat@eurotolia.com>
2023-01-16 10:05:33 +0300

    add script

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-calculateButton = document.getElementsByClassName('calculate');
+calculateButton = document.getElementById('calculate');
 
-calculateButton.addEventListener('click', function(){
-    visaNote = 
-})
+calculateButton.addEventListener('click', () => {
+    visaNoteElement = document.getElementById("visaNote");
+    passNoteElement = document.getElementById("passNote");
+    requiredNoteElement = document.getElementById("requiredNote");
+    requiredNote = calculatePassNote(visaNoteElement.value, passNote.value);
+    requiredNoteElement.textContent = "You need to take " + requiredNote + " pass";
+
+});
+
+
+let calculatePassNote = (visaNote, passNote) => {
+    return (passNote - (0.3 * visaNote)) * 0.8;
+}
